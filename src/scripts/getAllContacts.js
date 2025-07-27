a490c786405b889071af322d215571901e034a2b
Nastia Komisar <anastasiakomissar0302@gmail.com>
2025-07-27 17:16:08 +0300

    Added hw1

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,3 +1,15 @@
-export const getAllContacts = async () => {};
+import { readContacts } from "../utils/readContacts.js";
+
+export const getAllContacts = async () => {
+    const contacts = await readContacts();
+
+    if (contacts.length === 0) {
+        console.log("There are no contacts");
+        return;
+    }
+
+    console.log("All contacts:");
+    return contacts;
+};
 
 console.log(await getAllContacts());
